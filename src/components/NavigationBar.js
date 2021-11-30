787ac5eb9c496d884c03b034d7a4ce397db3a668
Heng <31088159+shiukaheng@users.noreply.github.com>
2021-11-30 19:25:51 +0800

    updated format

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,9 +4,10 @@ import Logo from "./Logo"
 // Everything horizontally centered using Tailwind
 export default function NavigationBar(props) {
     return (
-        <div className="flex flex-col justify-center gap-2 mb-4">
-            <div className="text-center font-serif text-lg">情迷香港 the lost metropolis</div>
-            <div className="flex justify-center">
+        <div className="flex flex-col gap-2 pr-16">
+            {/* <div className="text-center font-serif text-lg">情迷香港 the lost metropolis</div> */}
+            <Logo />
+            <div className="flex justify-center flex-col">
                 {props.children}
             </div>
             
