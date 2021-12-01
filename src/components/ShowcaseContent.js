434c62fe8ad8a9b9fe7998727eadaf2d4512ab1b
Heng <31088159+shiukaheng@users.noreply.github.com>
2021-12-01 17:56:43 +0800

    slide animated

--- src/components/ShowcaseContent.js
+++ src/components/ShowcaseContent.js
@@ -1,18 +1,16 @@
-import {v4 as uuidv4} from 'uuid';
 import ShowcaseContentCard from './ShowcaseContentCard';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import {ArrowLeftIcon, ArrowRightIcon} from '@heroicons/react/outline';
+import SwipeableViews from 'react-swipeable-views';
 
 const content_array = [
     {
-        "id": uuidv4(),
         "title_english": "The State Theatre Reborn",
         "title_chinese": "皇都再世",
         "description_english": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem. Quasi, quisquam.",
         "description_chinese": ""
     },
     {
-        "id": uuidv4(),
         "title_english": "The Salon",
         "title_chinese": "皇都再世 2",
         "description_english": " 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem. Quasi, quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem. Quasi, quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem. Quasi, quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem. Quasi, quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem. Quasi, quisquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem. Quasi, quisquam.",
@@ -22,24 +20,23 @@ const content_array = [
 
 function ShowcaseContent() {
     const [activeIndex, setActiveIndex] = useState(0);
-
-    // useEffect(() => {
-    //     setTimeout(
-    //         () =>
-    //             setActiveIndex((i) => (i + 1) % content_array.length)
-    //         , 10000
-    //     );
-    //     return () => {}
-    // }, [activeIndex])
-
     return (
-        <div className="w-full relative overflow-hidden flex flex-col flex-grow">
-            {/* <div className="absolute w-full h-full flex flex-row justify-end place-items-end md:place-items-start">
-                <button className="text-xl border border-black px-2 rounded-full h-9 w-9">List</button>
-            </div> */}
-            <ShowcaseContentCard content={content_array[activeIndex]}/>
-            {/* Navigating between different scans.. Todo swipe left and right for mobile. Hidden left and right arrows. Can use same animation for desktop. */}
-            <div className="flex flex-row gap-2 justify-between invisible md:visible">
+        <div className="flex flex-col h-full w-full justify-between">
+            <div className="h-full w-full relative overflow-hidden">
+                <SwipeableViews index={activeIndex} onChangeIndex={setActiveIndex} containerStyle={{
+                    position: 'absolute',
+                    height: '100%',
+                    width: '100%'
+                }}>
+                    {content_array.map((content, key) => (
+                        <div key={key} className="absolute w-full h-full overflow-y-auto">
+                            <ShowcaseContentCard content={content}/>
+                        </div>
+                    ))}
+                </SwipeableViews>
+            </div>
+             {/* Navigating between different scans.. Todo swipe left and right for mobile. Hidden left and right arrows. Can use same animation for desktop. */}
+            <div className="flex-row gap-2 justify-between hidden md:flex">
                 <ArrowLeftIcon className="h-5 cursor-pointer hover:opacity-50 transition-opacity duration-500" onClick={()=>(
                     setActiveIndex((i) => (i + 1) % content_array.length))
                     }></ArrowLeftIcon>
--- src/components/ShowcaseContentCard.js
+++ src/components/ShowcaseContentCard.js
@@ -1,9 +1,10 @@
 function ShowcaseContentCard({content}) {
     return ( 
-        <div className="flex flex-col h-full mt-72 md:mt-0 flex-grow">
+        <div className={"flex flex-col"}>
+            <div className="h-72 md:h-0"></div>
             <div className="flex flex-row mb-4 justify-between md:justify-start">
                 <div className="text-2xl md:text-4xl font-serif font-bold mr-4">{content.title_english}</div>
-                <button className="border-black border px-4 rounded-full hover:opacity-50 transition-opacity duration-500 font-serif font-bold text-s md:text-xl h-8 md:h-9">explore</button>
+                <button className="border-black border px-4 rounded-full md:hover:opacity-50 transition-opacity duration-500 font-serif font-bold text-s md:text-xl h-8 md:h-9">explore</button>
             </div>
             <div className="font-serif">
                 {content.description_english}
