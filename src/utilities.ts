b62b1580961349ab4530a4a1a39e2dd5e8752888
Heng <31088159+shiukaheng@users.noreply.github.com>
2022-01-23 12:53:08 +0800

    Changed utilities.js export method

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,4 +2,4 @@ function formatRGBCSS(color: number[]): string {
     return "rgb(" + color[0] + "," + color[1] + "," + color[2] + ")";
 }
 
-module.exports = { formatRGBCSS };
+export { formatRGBCSS };
