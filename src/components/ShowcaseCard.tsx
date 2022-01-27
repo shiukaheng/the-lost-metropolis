226f90c93f357b6f70168c52f43af471293198c0
Heng <31088159+shiukaheng@users.noreply.github.com>
2022-01-28 02:00:31 +0800

    Updated ShowcaseContentCard to use Experience component

--- src/components/ShowcaseCard.tsx
+++ src/components/ShowcaseCard.tsx
@@ -1,15 +1,19 @@
 import MagicDiv from "./MagicDiv";
+import { Fragment } from "react";
+import { useNavigate } from 'react-router-dom';
 
 function ShowcaseContentCard({content}) {
+    const navigate = useNavigate();
     return ( 
-        <div className={"flex flex-col page-margins md:pb-0"}>
-            <div className="h-72 md:h-0"></div>
-            <div className="flex flex-row mb-4 justify-between md:justify-start">
-                <MagicDiv className="text-2xl md:text-4xl font-serif font-bold mr-4" languageSpecificChildren={content.title}/>
-                <MagicDiv mergeTransitions={true} className="secondary-button" onClick={()=>{window.open(content.link)}} languageSpecificChildren={{en: "explore", zh: "探索"}}/>
+        <Fragment>
+            <div className={"flex flex-col page-margins md:pb-0"}>
+                <div className="flex flex-row mb-4 justify-between md:justify-start pt-[72px] md:pt-0">
+                    <MagicDiv className="text-2xl md:text-4xl font-serif font-bold mr-4" languageSpecificChildren={content.title}/>
+                    <MagicDiv mergeTransitions={true} className="secondary-button" onClick={()=>{navigate(`/experience/${content.id}`)}} languageSpecificChildren={{en: "explore", zh: "探索"}}/>
+                </div>
+                <MagicDiv className="font-serif max-w-xl" languageSpecificChildren={content.description}/>
             </div>
-            <MagicDiv className="font-serif max-w-xl" languageSpecificChildren={content.description}/>
-        </div>
+        </Fragment>
     );
 }
 
