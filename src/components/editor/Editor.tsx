aee23c4d65fbf9098bd19649449730c306455e3a
Heng <31088159+shiukaheng@users.noreply.github.com>
2022-01-31 18:55:10 +0800

    expanded EditorContext

--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -92,7 +92,7 @@ editorRegister(DepthKitObject, {
     },
 })
 
-function _updatePartialSceneChildren(sceneChildren, childrenToUpdate) {
+function joinChildren(sceneChildren, childrenToUpdate) {
     // return new sceneChildren but with the children whose props.id is in updateChildren
     const idsToBeUpdated = childrenToUpdate.map(child => child.props.id)
     return sceneChildren.map(child => {
@@ -104,31 +104,70 @@ function _updatePartialSceneChildren(sceneChildren, childrenToUpdate) {
     })
 }
 
-const defaultEditorContext = {
-    childrenInteractions: true,
+// Todo: Move common editor children props to context, including: 
+// sceneChildren, setSceneChildren, addSceneChildren, removeSceneChildren,
+// selectedIDs, setSelectedIDs, addSelectedIDs, removeSelectedIDs,
+// transformMode, setTransformMode,
+// transformSpace, setTransformSpace,
+// overrideInteractions, setOverrideInteractions
+
+defaultEditorContext = {
+    sceneChildren: [],
+    setSceneChildren: () => { },
+    addSceneChildren: () => { },
+    removeSceneChildren: () => { },
+    selectedIDs: [],
+    setSelectedIDs: () => { },
+    addSelectedIDs: () => { },
+    removeSelectedIDs: () => { },
+    transformMode: "translate",
+    setTransformMode: () => { },
+    transformSpace: "world",
+    setTransformSpace: () => { },
+    overrideInteractions: false,
+    setOverrideInteractions: () => { },
 }
 
 const EditorContext = createContext(defaultEditorContext)
 
 function Editor() {
+    // Setup state for editor
     const [sceneChildren, _setSceneChildren] = useState([])
     const [selectedIDs, setSelectedIDs] = useState([])
     const [transformMode, setTransformMode] = useState("translate")
     const [transformSpace, setTransformSpace] = useState("world")
-    const [childrenInteractions, setChildrenInteractions] = useState(true)
+    const [overrideInteractions, setOverrideInteractions] = useState(true)
+
+    // Make selectedIDs react to setSceneChildren
     const setSceneChildren = (newChildren) => {
         const sceneIDs = newChildren.map(child => child.props.id)
         setSelectedIDs(selectedIDs.filter(id => sceneIDs.includes(id)))
         _setSceneChildren(newChildren)
     }
-    const updatePartialSceneChildren = (newChildren) => {
-        _setSceneChildren(_updatePartialSceneChildren(sceneChildren, newChildren))
+
+    // Create convenience functions for adding and removing children
+    const addSceneChildren = (newChildren) => {
+        _setSceneChildren(joinChildren(sceneChildren, newChildren))
     }
+    const removeSceneChildren = (childrenToRemove) => {
+        _setSceneChildren(sceneChildren.filter(child => !childrenToRemove.includes(child)))
+        // Remove selected children
+        setSelectedIDs(selectedIDs.filter(id => !childrenToRemove.map(child => child.props.id).includes(id)))
+    }
+
+    // Create convenience functions for adding and removing selectedIDs
+    const addSelectedIDs = (newIDs) => {
+        setSelectedIDs(selectedIDs.concat(newIDs))
+    }
+    const removeSelectedIDs = (idsToRemove) => {
+        setSelectedIDs(selectedIDs.filter(id => !idsToRemove.includes(id)))
+    }
+
     // Wrap children whose child.props.id is in selectedIDs with TransformControls
     const wrappedSceneChildren = sceneChildren.map(child => {
         if (selectedIDs.includes(child.props.id)) {
             return (
-                <EditorTransformControls updatePartialSceneChildren={updatePartialSceneChildren} mode={transformMode} space={transformSpace} key={child.props.id}>
+                <EditorTransformControls updatePartialSceneChildren={addSceneChildren} mode={transformMode} space={transformSpace} key={child.props.id}>
                     {child}
                 </EditorTransformControls>
             )
@@ -138,23 +177,27 @@ function Editor() {
     })
 
     return (
-        <MagicDiv backgroundColorCSSProps={["backgroundColor"]} className="absolute w-full h-full flex flex-row">
-            <div className="w-1/2 h-full flex flex-col p-4 overflow-clip">
-                <div className="editor-embedded-widget text-2xl font-bold">Editor</div>
-                <EditorComponentGraph sceneChildren={sceneChildren} setSceneChildren={setSceneChildren} selectedIDs={selectedIDs} setSelectedIDs={setSelectedIDs} supportedComponents={supportedComponents}/>
-                <EditorComponentProperties sceneChildren={sceneChildren} setSceneChildren={setSceneChildren} selectedIDs={selectedIDs} supportedComponents={supportedComponents}/>
-                <EditorTransformOptions {...{transformMode, setTransformMode, transformSpace, setTransformSpace}}/>
-                <EditorIO sceneChildren={sceneChildren} setSceneChildren={setSceneChildren}/>
-            </div>
-            <div className="w-1/2 h-full bg-black">
-                <DebugViewport className="w-full h-full">
-                    <DebugPlane rotation={[Math.PI/2, 0, 0]}/>
-                    <EditorContext.Provider value={{childrenInteractions: childrenInteractions}}>
-                        {wrappedSceneChildren}
-                    </EditorContext.Provider>
-                </DebugViewport>
-            </div>
-        </MagicDiv>
+        <EditorContext.Provider value={
+            {sceneChildren, setSceneChildren, addSceneChildren, removeSceneChildren, selectedIDs, setSelectedIDs, addSelectedIDs, removeSelectedIDs, transformMode, setTransformMode, transformSpace, setTransformSpace, overrideInteractions, setOverrideInteractions}
+        }>
+            <MagicDiv backgroundColorCSSProps={["backgroundColor"]} className="absolute w-full h-full flex flex-row">
+                <div className="w-1/2 h-full flex flex-col p-4 overflow-clip">
+                    <div className="editor-embedded-widget text-2xl font-bold">Editor</div>
+                    <EditorComponentGraph sceneChildren={sceneChildren} setSceneChildren={setSceneChildren} selectedIDs={selectedIDs} setSelectedIDs={setSelectedIDs} supportedComponents={supportedComponents}/>
+                    <EditorComponentProperties sceneChildren={sceneChildren} setSceneChildren={setSceneChildren} selectedIDs={selectedIDs} supportedComponents={supportedComponents}/>
+                    <EditorTransformOptions {...{transformMode, setTransformMode, transformSpace, setTransformSpace}}/>
+                    <EditorIO sceneChildren={sceneChildren} setSceneChildren={setSceneChildren}/>
+                </div>
+                <div className="w-1/2 h-full bg-black">
+                    <DebugViewport className="w-full h-full">
+                        <DebugPlane rotation={[Math.PI/2, 0, 0]}/>
+                        <EditorContext.Provider value={{childrenInteractions: overrideInteractions}}>
+                            {wrappedSceneChildren}
+                        </EditorContext.Provider>
+                    </DebugViewport>
+                </div>
+            </MagicDiv>
+        </EditorContext.Provider>
     );
 }
 
