18402295728cfd7c492de5ade93052ff84d3e77e
Ka Heng Shiu <31088159+shiukaheng@users.noreply.github.com>
2022-03-16 19:01:22 +0800

    removed irrelevant code

--- src/components/development/UploadAssetTest.tsx
+++ src/components/development/UploadAssetTest.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { uploadAsset } from "../../api";
+// import { uploadAsset } from "../../api";
 import GenericPage from "../utilities/GenericPage";
 import MagicButton from "../utilities/MagicButton";
 
@@ -8,12 +8,12 @@ export default function UploadAssetPage(){
     const [file, setFile] = useState(null);
     return (
         <GenericPage className="flex flex-col gap-4">
-            <div className="text-5xl font-black">upload asset</div>
+            {/* <div className="text-5xl font-black">upload asset</div>
             <input type="file" onChange={(e)=>setFile(e.target.files[0])}/>
             <MagicButton onClick={async ()=>{
                 const id = await uploadAsset(file, true, (p)=>{console.log(p)})
                 console.log("uploaded", id)
-            }}>upload</MagicButton>
+            }}>upload</MagicButton> */}
         </GenericPage>
     )
 }
