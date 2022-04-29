ca4b028767fb27036564c7205e3ae181cb923bf9
Heng <31088159+shiukaheng@users.noreply.github.com>
2022-04-29 19:59:55 +0800

    updated image.ts

---
+++ functions/src/lib/types/asset_types/Image.ts
@@ -0,0 +1,20 @@
+import { AssetLiteral } from "../../../../../api/types/AssetLiteral";
+import { AssetConverterFunction, AssetType } from "../AssetType";
+import * as fs from "fs";
+
+export class Image implements AssetType {
+    static conversionMap: Map<typeof AssetType, AssetConverterFunction> = new Map();
+    static assetLiteral: AssetLiteral = "Image";
+    static target: true;
+    static source: true;
+    static validate(assetData: object, rootPath: string): void {
+        // Check that rootPath is actually a directory
+        if (!fs.lstatSync(rootPath).isDirectory()) {
+            throw `rootPath ${rootPath} is not a directory`
+        }
+        // TODO: Implement the proper checks
+    }
+    static getConverter(targetAssetType: typeof AssetType): AssetConverterFunction | undefined {
+        return Image.conversionMap.get(targetAssetType);
+    }
+}
