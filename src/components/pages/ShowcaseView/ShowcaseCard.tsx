b16e68996ee7b1b024f99deade651557fb7f98fa
Ka Heng Shiu <31088159+shiukaheng@users.noreply.github.com>
2022-04-30 19:58:26 +0800

    prepped ShowcaseCard for sponsors

--- src/components/pages/ShowcaseView/ShowcaseCard.tsx
+++ src/components/pages/ShowcaseView/ShowcaseCard.tsx
@@ -9,12 +9,14 @@ import { Post } from "../../../../api/types/Post";
 function ShowcaseContentCard({post}: {post: Instance<Roled<Post>>}) {
     const navigate = useNavigate();
     return ( 
-        <div className={"w-full h-full flex flex-col page-margins"}>
+        <div className={"w-full h-full flex flex-col page-margins md:pb-0"}>
             <div className="flex flex-row mb-4 gap-2 md:gap-4 justify-between md:justify-start pt-[72px] md:pt-0">
                 <MagicDiv className="text-2xl md:text-4xl font-serif font-bold" languageSpecificChildren={post.data.title}/>
                 <MagicButton className="h-8 md:h-10" onClick={()=>{navigate(`/view/${post.id}`)}} languageSpecificChildren={{en: "explore", zh: "探索"}}/>
             </div>
             <MagicDiv className="font-serif max-w-xl" languageSpecificChildren={post.data.description}/>
+            <div className="grow"/>
+            {/* Add sponsors here */}
         </div>
     );
 }
