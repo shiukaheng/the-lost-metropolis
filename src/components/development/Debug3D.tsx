72a3580ccef8d9d39031e782ebbbf9d0b6cd1029
Heng <31088159+shiukaheng@users.noreply.github.com>
2022-05-24 16:17:39 +0800

    recreated parallel

--- src/components/development/Debug3D.tsx
+++ src/components/development/Debug3D.tsx
@@ -1,14 +1,20 @@
 import { VRCanvas } from "@react-three/xr";
+import { ProtoTilesObject } from "../3d/subcomponents/ProtoTilesObject";
 import { TilesObject } from "../3d/TilesObject";
 import GameControls from "../utilities/GameControls";
 import { Viewer } from "../viewer/Viewer";
 
 function Debug3D() {
     return (
-        <div className="absolute w-full h-full bg-black">
+        <div className="absolute w-full h-full">
             <VRCanvas className="w-full h-full viewport-canvas">
                 <GameControls/>
-                <TilesObject objectID="test" scale={0.2} url={"https://storage.googleapis.com/the-lost-metropolis-production-static/salon_xs/tileset.json"}/>
+                {/* <CameraPosSetter/> */}
+                <mesh rotation={[Math.PI/2, 0, 0]}>
+                    <planeBufferGeometry attach="geometry" args={[10, 10]} />
+                    <meshBasicMaterial attach="material" wireframe color="red" />
+                </mesh>
+                <ProtoTilesObject scale={0.2} url={"https://storage.googleapis.com/the-lost-metropolis-production-static/salon_xs/tileset.json"}/>
             </VRCanvas>
         </div>
     );
