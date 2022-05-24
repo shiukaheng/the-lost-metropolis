e4f29f46b387b64288fdea481106fbfa927e8952
Ka Heng Shiu <31088159+shiukaheng@users.noreply.github.com>
2022-05-24 18:58:30 +0800

    Revert "Merge branch 'xr-tiles-dev' of https://github.com/shiukaheng/the-lost-metropolis into xr-tiles-dev"
    This reverts commit 97fea5c0b94d636dff3da55506286bf08f2cf474, reversing
    changes made to 9fa590f3ada4aadc57c0096f8373dcc925510552.

--- src/components/development/Debug3D.tsx
+++ src/components/development/Debug3D.tsx
@@ -1,20 +1,14 @@
 import { VRCanvas } from "@react-three/xr";
-import { ProtoTilesObject } from "../3d/subcomponents/ProtoTilesObject";
 import { TilesObject } from "../3d/TilesObject";
 import GameControls from "../utilities/GameControls";
 import { Viewer } from "../viewer/Viewer";
 
 function Debug3D() {
     return (
-        <div className="absolute w-full h-full">
+        <div className="absolute w-full h-full bg-black">
             <VRCanvas className="w-full h-full viewport-canvas">
                 <GameControls/>
-                {/* <CameraPosSetter/> */}
-                <mesh rotation={[Math.PI/2, 0, 0]}>
-                    <planeBufferGeometry attach="geometry" args={[10, 10]} />
-                    <meshBasicMaterial attach="material" wireframe color="red" />
-                </mesh>
-                <ProtoTilesObject scale={0.2} url={"https://storage.googleapis.com/the-lost-metropolis-production-static/salon_xs/tileset.json"}/>
+                <TilesObject objectID="test" scale={0.2} url={"https://storage.googleapis.com/the-lost-metropolis-production-static/salon_xs/tileset.json"}/>
             </VRCanvas>
         </div>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,10 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './components/App';
-import Debug3D from './components/development/Debug3D';
 // import Debug3D from "./components/Debug3D";
 // import { Editor } from "./components/editor/Editor"
 // import Login from './components/admin/Login';
 
 // ReactDOM.render(<Editor/>, document.getElementById('root')); 
-ReactDOM.render(<Debug3D/>, document.getElementById('root')); 
+ReactDOM.render(<App/>, document.getElementById('root')); 
 // ReactDOM.render(<Debug3D/>, document.getElementById('root'));
