d3f0de4b6bfcffbc7215c04ee7658dee05f5dbd4
Ka Heng Shiu <31088159+shiukaheng@users.noreply.github.com>
2022-06-02 21:36:06 +0800

    defined TeleportTargetComponent type

---
+++ src/components/3d/template_types.ts/TeleportTargetComponent.ts
@@ -0,0 +1,9 @@
+export type TargetEffect = "target" | "blocker" | "bypass"
+
+// "target" will allow the user to teleport to the target
+// "blocker" will block the teleporter ray and display as an invalid target
+// "bypass" will have no effect on the raycast, will lead to better performance as the raycast will not be performed
+
+export type TeleportTargetComponentProps = {
+    effect: TargetEffect
+}
